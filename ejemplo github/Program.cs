8bb541150ed0efce08240dfb0beefa29ebf6ec29
El juan <lopezrodriguezj287@gmail.com>
2022-09-17 14:52:49 -0600

    feat. codigos

--- ejemplo github/Program.cs
+++ ejemplo github/Program.cs
@@ -14,7 +14,7 @@ namespace ejemplo_github
             Console.ForegroundColor = ConsoleColor.Gray;
 
             string producto = "";
-
+            string codigo = "";
             double precio = 0;
 
             int cantidad = 0;
@@ -22,13 +22,16 @@ namespace ejemplo_github
             Console.WriteLine("Ingrese su producto.");
             producto = Console.ReadLine();
 
+            Console.WriteLine(" ingrese el codigo del producto");
+            codigo = Console.ReadLine();
+
             Console.WriteLine("Precio: ");
             precio = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Ingrese la cantidad de producto: ");
             cantidad = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("El producto {0} tiene una inversion de {1}", producto, (double)precio * cantidad);
+            Console.WriteLine("El producto {0} con codigo de {1} tiene una inversion de {2}", producto,codigo, (double)precio * cantidad);
 
             Console.ReadKey();
 
